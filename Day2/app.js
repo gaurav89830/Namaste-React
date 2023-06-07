4f212a8f9f8b93409a783005002afb54db76106a
brokeguy <gauravsharma@Gauravs-MacBook-Air.local>
2023-06-08 03:05:16 +0530

    DAY 2

---
+++ Day2/app.js
@@ -0,0 +1,44 @@
+
+/**
+ * 
+ * PARCER : 
+ * HMR - HOT MODULE REPLACEMENT
+ * File Watcher Algo - C++ ( checks the changes in files)
+ * Bundler / minify
+ * cleaning code
+ * image optimization
+ * caching while development
+ * HTTPS on dev
+ * consistent hashing algos
+ * 
+ */
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement(
+  "h1",
+  {
+    className: "newClass",
+  },
+  "first react app"
+);
+const heading2 = React.createElement(
+  "h1",
+  {
+    className: "newClass",
+  },
+  "Hello world"
+);
+
+const main = React.createElement(
+  "h1",
+  {
+    className: "hehe",
+  },
+  [heading, heading2]
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(main);
